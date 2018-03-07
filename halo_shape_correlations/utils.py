2f78102cf83aac773c22f191a5730b388ffcc92b
Duncan Campbell <duncanc@andrew.cmu.edu>
2018-03-07 16:07:53 -0500

    added utils

--- halo_shape_correlations/utils.py
+++ halo_shape_correlations/utils.py
@@ -5,13 +5,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import numpy as np
 
-_all__ = ('empirical_cdf', 'binned_bootstrap')
+_all__ = ('empirical_cdf', 'binned_bootstrap_1d')
 
 __author__=['Duncan Campbell']
 
 def empirical_cdf(x, bins=None):
     """
-    calculate the emprical cumulativr distribution
+    calculate the emprical cumulative distribution
 
     Parameters
     ----------
@@ -28,24 +28,21 @@ def empirical_cdf(x, bins=None):
 
     x = np.sort(x)
 
-    if bins is None:
-        cdf = np.ones(len(x))
-        cdf = np.cumsum(cdf)/np.sum(cdf)
-    else:
+    cdf = np.ones(len(x))
+    cdf = np.cumsum(cdf)/np.sum(cdf)
+    if bins is not None:
         bins = np.sort(bins)
         inds = np.searchsorted(x, bins)
-        x = x[inds]
-        # pad cdf with zero at beginning
-        cdf = np.ones(len(x)+1)
-        cdf[0]=0
-        cdf = np.cumsum(cdf)/np.sum(cdf)
-
+        if np.max(inds) == len(x):
+            cdf = np.append(cdf, [1.0])
+        if np.min(inds) == 0:
+            cdf = np.insert(cdf, [0], [0])
         cdf = cdf[inds]
 
-    return cdf, x
+    return cdf, bins
 
 
-def binned_bootstrap_1D(x, bins, statistic='mean', Nboot=100):
+def binned_bootstrap_1d(x, values, bins, statistic='mean', Nboot=100):
     """
     calculate the mean value and standard deviation of a statistic calculated in bins by bootstrapping.
     This function is modelled off of scipy.stats.binned_statistic.
@@ -53,6 +50,10 @@ def binned_bootstrap_1D(x, bins, statistic='mean', Nboot=100):
     Parameters
     ----------
     x : array_like
+        A sequence of values to be binned.
+
+    values : array_like
+        The values on which the statistic will be computed. This must be the same shape as x.
 
     bins : array_like
 
@@ -72,21 +73,27 @@ def binned_bootstrap_1D(x, bins, statistic='mean', Nboot=100):
     Examples
     --------
     First, define some random data:
+
     >>> x = np.random.random(100)
+    >>> values = np.random.random(100)
 
     Define some bins:
+
     >>> bins = np.linspace(0,1,11)
 
     Find the mean in each bin and estimate the error on the mean by bootstrap sampling 100 times:
-    >>> mean_in_bins, err = binned_bootstrap_1D(x, bins, statistic='mean', Nboot=100)
+
+    >>> mean_in_bins, err = binned_bootstrap_1D(x, values, bins, statistic='mean', Nboot=100)
     """
 
     edges = np.asarray(bins, float)
-    nbin = np.asarray(len(edges) + 1)  # +1 for outlier bins
+    nbin = len(edges) - 1
     dedges = np.diff(edges)
 
     # Compute the bin number each element in `x` falls into
-    bin_number = np.digitize(x, bins)
+    bin_number = np.digitize(x, edges)
+
+    values = np.asarray(values)
 
     # Using `digitize`, values that fall on an edge are put in the right bin.
     # For the rightmost bin, we want values equal to the right
@@ -94,40 +101,54 @@ def binned_bootstrap_1D(x, bins, statistic='mean', Nboot=100):
     # Find the rounding precision
     decimal = int(-np.log10(dedges.min())) + 6
     # Find which points are on the rightmost edge.
-    on_edge = np.where(np.around(sample[:], decimal) == np.around(edges[-1], decimal))[0]
+    on_edge = np.where(np.around(x, decimal) == np.around(edges[-1], decimal))[0]
     # Shift these points one bin to the left.
     bin_number[on_edge] -= 1
 
     result = np.empty((Nboot, nbin), float)
 
-    # loop through each bin
-    for i in range(1, len(bins)):
-        mask = (bin_number==i)
-        xx = x[mask]
-        for j in range(0, Nboot):
-            xxx = np.random.choice(xx, size=len(xx), replace=True)
+    # loop through each bootstrap sample
+    for i in range(0, Nboot):
+        boot_inds = np.random.randint(0, len(x), size=len(x))
+        y = values[boot_inds]  # bootstrapped sample of values
+        # loop through each bin
+        for j in range(0, len(bins)-1):
+            mask = (bin_number[boot_inds]==(j+1))
+            yy = y[mask]  # bootstrapped sample of xx within the bin
             if statistic == 'mean':
-                result[j, i] = np.mean(xxx)
+                result[i, j] = np.nan
+                result[i, j] = np.mean(yy)
             elif statistic == 'std':
-                result[j, i] = np.std(xxx)
+                result[i, j] = 0.0
+                result[i, j] = np.std(yy)
             elif statistic == 'count':
-                result[j, i] = len(xx)
+                result[i, j] = 0
+                result[i, j] = len(yy)
             elif statistic == 'sum':
-                result[j, i] = np.sum(xxx)
+                result[i, j] = 0.0
+                result[i, j] = np.sum(yy)
             elif statistic == 'median':
-                result[j, i] = np.median(xxx)
+                result[i, j] = np.nan
+                result[i, j] = np.median(yy)
             elif statistic == 'min':
-                result[j, i] = np.min(xxx)
+                result[i, j] = np.nan
+                result[i, j] = np.min(yy)
             elif statistic == 'max':
-                result[j, i] = np.max(xxx)
+                result[i, j] = np.nan
+                result[i, j] = np.max(yy)
             elif callable(statistic):
-                result[j, i] = statistic(xxx)
+                try:
+                    null = statistic([])
+                except:
+                    null = np.nan
+                result[i, j] = null
+                result[i, j] = statistic(yy)
             else:
                 msg = ('`statistic` must be a callable function.')
                 raise ValueError(msg)
 
-    mean_result = np.mean(result, axis=0)
-    err = np.std(resutl, axis=0)
+    mean_result = np.nanmean(result, axis=0)
+    err = np.nanstd(result, axis=0)
 
     return mean_result, err
 
@@ -158,6 +179,9 @@ def symmetrize_angular_distribution(theta, radians=True):
     uran = np.random.random(len(costheta_1))
     result = np.pi + dtheta
     result[uran < 0.5] = -1.0*result[uran < 0.5]
+
+    if not radians:
+        theta = np.degrees(theta)
 
     return result
 
