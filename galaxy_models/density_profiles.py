bbd26cf00e1665e9afcf85292ce55b12a59ee04d
Duncan Campbell <duncan@donut-hole.local>
2018-09-18 16:46:24 -0400

    added sampling feature

--- galaxy_models/density_profiles.py
+++ galaxy_models/density_profiles.py
@@ -6,15 +6,15 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import numpy as np
 from scipy.special import hyp2f1  # hypergeometric function
-from scipy.special import betainc
-from scipy.stats import beta
+from scipy.special import betainc, beta
+from scipy.special import btdtri as beta_inv
 
 
 class cusped_density(object):
     """
     """
 
-    def __init__(self, gamma=1, n=4, b_to_a=1.0, c_to_a=1.0):
+    def __init__(self, gamma=1, n=4, b_to_a=1.0, c_to_a=1.0, mtot=1.0):
         """
         """
 
@@ -23,7 +23,8 @@ class cusped_density(object):
         self.a = 1.0
         self.b = self.a * b_to_a
         self.c = self.a * c_to_a
-        self.rho0 = 1.0
+        self.mtot = mtot
+        self.rho0 = self.central_density()
 
     def enclosed_mass(self, m):
         """
@@ -32,16 +33,63 @@ class cusped_density(object):
         a = self.a
         b = self.b
         c = self.c
-        gamma = self.gamma
-        f2 = hyp2f1(self.n-gamma, 3-gamma, 4-gamma, m/(m+1.0))
 
-        return 4.0*np.pi*a*b*c*(m/(m+1.0))**(3-gamma)*(1/(3-gamma))*f2
+        gamma = self.gamma
+        n = self.n
+        mtot = self.mtot
+
+        rho0 = self.central_density()
+
+        return 4.0*np.pi*a*b*c*rho0*betainc(n-3, 3-gamma, m/(m+1.0))*beta(n-3, 3-gamma)
+
+    def central_density(self):
+        """
+        """
+        
+        a = self.a
+        b = self.b
+        c = self.c
+
+        gamma = self.gamma
+        n = self.n
+        mtot = self.mtot
+
+        return mtot/(4.0*np.pi*a*b*c*beta(n-3, 3-gamma))
+
 
     def sample(self, size):
         """
+        returned axis algined particles drawn from density profile.
+        - major axis is aligned with the x-axis
+        - intermediate with the y-axis
+        - minor axis with the z-axis
         """
 
-        if self.n > 3:
-            beta.ppf(ranf,self.n-3, 3-self.gamma)
+        gamma = self.gamma
+        n = self.n
+        mtot = self.mtot
+
+        a = self.a
+        b = self.b
+        c = self.c
+        
+        rho0 = self.central_density()
+        k = 4.0*np.pi*a*b*c*beta(n-3, 3-gamma)     
+
+        f = np.random.random(size)
+
+        ff = beta_inv(n-3, 3-gamma, f*mtot/(rho0*k)) 
+        m = -1.0*(ff/(ff-1.0))
+
+        phi = np.random.uniform(0, 2*np.pi, size)
+        cos_t = np.random.rand(size)*2 - 1
+        sin_t = np.sqrt((1.0-cos_t*cos_t))
+
+        x = a*m * np.cos(phi)
+        y = b*m * np.sin(phi)
+        z = c*m * cos_t
+
+        return np.vstack((x,y,z)).T
+        
 
 
