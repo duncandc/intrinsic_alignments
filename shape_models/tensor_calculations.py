a7e7150850b9b94f82fea00b86bcb97bd62c575f
Duncan Campbell <duncan.campbell@yale.edu>
2018-09-20 17:36:13 -0400

    added option to calculate reduced tensor

--- shape_models/tensor_calculations.py
+++ shape_models/tensor_calculations.py
@@ -10,7 +10,7 @@ __author__ = ('Duncan Campbell')
 
 
 def principal_axes_from_inertia_tensors(inertia_tensors):
-    r""" 
+    r"""
     Calculate the principal eigenvector of each of the input inertia tensors.
 
     Parameters
@@ -30,14 +30,14 @@ def principal_axes_from_inertia_tensors(inertia_tensors):
     Notes
     -----
     """
-    
+
     ndim = np.shape(inertia_tensors)[-1]
 
     evals, evecs = np.linalg.eigh(inertia_tensors)
     return evecs[:, :, ndim-1], evals[:, ndim-1]
 
 
-def inertia_tensors(x, weights=None):
+def inertia_tensors(x, weights=None, reduced=True):
     r"""
     Calculate the n1 inertia tensors for a set of n2 points of dimension ndim.
 
@@ -49,6 +49,9 @@ def inertia_tensors(x, weights=None):
 
     weights :  ndarray
         Numpy array of shape (n1, n2) storing n1 sets of n2 weights
+
+    reduced : boolean
+        bool indicating whether to calculate the reduced or unreduced inertia tensor
     """
 
     n1, n2, ndim = np.shape(x)
@@ -60,8 +63,12 @@ def inertia_tensors(x, weights=None):
         msg = ('weights array misty nbe of shape (n1,n2)')
         raise ValueError(msg)
 
-    # need to implement weights still
-
-    return np.einsum('...ij,...ik->...jk',x,x)
+    if reduced:
+        r_squared = np.sum(x**2, -1)
+        I = np.einsum('...ij,...ik->...jk', x/(r_squared[:,:,np.newaxis]), x*weights[:,:,np.newaxis])
+        return I/(np.ones((n1,ndim,ndim))*n2)
+    else:
+        I = np.einsum('...ij,...ik->...jk', x, x*weights)
+        return I/(np.ones((n1,ndim,ndim))*n2)
 
 
